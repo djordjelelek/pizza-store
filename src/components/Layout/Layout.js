194ca0be8549d2a883e3d1883c30e24269af9180
Djordje Lelek <djordje.lelek@gmail.com>
2020-12-21 12:34:53 +0100

    add folder components(for stateles components) with Layout class, and containers(for statefull) with PizzaBulder class. Reorganize code.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Aux from "../../hoc/Auxilliary";
+import classes from "./Layout.module.css";
+
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdroop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
---
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxilliary";
+
+class PizzaBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Pizza</div>
+        <div>Build Control</div>
+      </Aux>
+    );
+  }
+}
+
+export default PizzaBuilder;
