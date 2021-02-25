b61355fa298225928d904c43fddf26a2565289b1
Djordje Lelek <djordje.lelek@gmail.com>
2021-02-25 16:03:07 +0100

    update mobile header

--- src/Header/SideDrawer/SideDrawer.js
+++ src/Header/SideDrawer/SideDrawer.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "../../AuthContext/AuthContext";
 import menu from "./menu-mobile.svg.png";
-import Menu from "../Menu/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles({
   list: {
     width: 250,
-    backgroundColor: "orange",
   },
   MenuIcon: {
     width: "3.1em",
@@ -28,6 +31,12 @@ const useStyles = makeStyles({
 export default function TemporaryDrawer() {
   const classes = useStyles();
   const [drawer, setDrawer] = React.useState(false);
+  const [loading, setLoading] = useState(false);
+  const { logIn } = useAuth();
+  const { setLogIn } = useAuth();
+  const { setToken } = useAuth();
+  const { setUserId } = useAuth();
+  const { cart } = useAuth();
 
   const toggleDrawer = (open) => (event) => {
     setDrawer(open);
@@ -40,8 +49,53 @@ export default function TemporaryDrawer() {
       onClick={toggleDrawer(false)}
       onKeyDown={toggleDrawer(false)}
     >
-      <Menu />
+      <List className={classes.ListItem}>
+        {[
+          <NavLink to="/home" activeStyle={{ color: "orange" }}>
+            Pizza Builder
+          </NavLink>,
+          <NavLink to="/orders-history" activeStyle={{ color: "orange" }}>
+            Orders History
+          </NavLink>,
+          <NavLink to="/cart" activeStyle={{ color: "orange" }}>
+            Cart
+          </NavLink>,
+        ].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
       <Divider />
+      <List>
+        {[
+          <NavLink to="/login" activeStyle={{ color: "orange" }}>
+            Log In
+          </NavLink>,
+          <NavLink
+            to="/login"
+            activeStyle={{ color: "orange" }}
+            onClick={() => {
+              setLoading(true);
+              setTimeout(() => {
+                setLogIn(false);
+                setToken("");
+                setUserId("");
+                sessionStorage.removeItem("token");
+                sessionStorage.removeItem("userId");
+                sessionStorage.removeItem("cart");
+                window.location.reload();
+              }, 500);
+            }}
+          >
+            Log Out
+          </NavLink>,
+        ].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 
